ce671a1eab6d8ef77c05aeb54ee45c88fe0f6c9c
Aditya-Singh-cs <aditya76596@gmail.com>
2025-03-08 11:24:08 +0530

    Added application properties for developer and production

--- src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollAppApplication.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollAppApplication.java
@@ -4,14 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 @Slf4j
 public class EmployeePayrollAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EmployeePayrollAppApplication.class, args);
-		log.info("Employee Payroll App Started");
+		ApplicationContext context= SpringApplication.run(EmployeePayrollAppApplication.class, args);
+//		log.info("Employee Payroll App Started");
+		log.info("Employee Payroll App started in {} environment", context.getEnvironment().getProperty("environment"));
 	}
 }
 
